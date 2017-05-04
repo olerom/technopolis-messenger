8fe325302cdebca4b01c5ae9dfeaf840e20536f4
olerom <mefiader@gmail.com>
2017-05-04 20:54:25 +0300

    Add resources with host and port properties

--- src/main/java/edu/technopolis/homework/messenger/User.java
+++ src/main/java/edu/technopolis/homework/messenger/User.java
@@ -5,4 +5,6 @@ package edu.technopolis.homework.messenger;
  */
 public class User {
     private long id;
+    private String login;
+    private String password;
 }
--- src/main/java/edu/technopolis/homework/messenger/net/MessengerServer.java
+++ src/main/java/edu/technopolis/homework/messenger/net/MessengerServer.java
@@ -1,24 +1,47 @@
 package edu.technopolis.homework.messenger.net;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Arrays;
+import java.util.Properties;
 
 /**
  *
  */
 public class MessengerServer {
-    private static final int PORT = 19000;
+    private int port;
 
     public static void main(String[] args) {
+        new MessengerServer().run();
+    }
+
+    private void run() {
+        initPort();
+
         try {
-            ServerSocket serverSocket = new ServerSocket(PORT);
+            ServerSocket serverSocket = new ServerSocket(port);
+
             while (true) {
                 Socket socket = serverSocket.accept();
                 new Thread(new RunnableServerLogic(socket)).start();
             }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initPort() {
+        try (InputStream input = new FileInputStream("./src/main/resources/portConfiguration.properties")) {
+
+            Properties properties = new Properties();
+            properties.load(input);
+
+            this.port = Integer.valueOf(properties.getProperty("port"));
+
         } catch (IOException e) {
             e.printStackTrace();
         }
